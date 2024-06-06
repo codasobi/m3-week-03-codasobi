09932e777117be7a28167c5de7a61c328f08e486
codasobi <154930553+codasobi@users.noreply.github.com>
2024-06-06 06:20:20 +0000

    feat: add confirm email

--- form_validator/form.js
+++ form_validator/form.js
@@ -5,24 +5,25 @@ const password = document.querySelector('#user-password')
 const passwordConfirm = document.querySelector('#user-password-confirm')
 
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault()
     checkLength(username, 3, 15)
     checkLength(password, 6, 25)
+    confirmEmail(email)
 })
 
-function checkLength (input, min, max) {
+function checkLength(input, min, max) {
     if (input.value.trim().length > max || input.value.trim().length < min) {
         const column = input.parentElement.querySelector('label').innerText.trim()
         showError(input, `${column} must be between ${min} - ${max} characters`)
     } else {
-        showSuccess (input)
+        showSuccess(input)
     }
 }
 
-function showError (input, msg) {
+function showError(input, msg) {
     const formGroup = input.parentElement
-    const Error = formGroup.querySelector("small");
+    const Error = formGroup.querySelector("small")
     // add red border hint
     input.classList.remove('valid')
     input.classList.add('invalid')
@@ -31,12 +32,21 @@ function showError (input, msg) {
     Error.classList.add('invalid')
 }
 
-function showSuccess (input) {
+function showSuccess(input) {
     const formGroup = input.parentElement
-    const Error = formGroup.querySelector("small");
+    const Error = formGroup.querySelector("small")
     // add green border hint
     input.classList.remove('invalid')
     input.classList.add('valid')
     // hide error msg
     Error.classList.remove('invalid')
+}
+
+function confirmEmail(input) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!emailRegex.test(input.value)) {
+        showError(input, `It is a not valid email address.`);
+    } else {
+        showSuccess(input)
+    }
 }
